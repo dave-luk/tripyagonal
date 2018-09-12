538a91143ffba076b1ca42b0af3579d3a6f48b51
dluk <dluk@avery.com>
2018-09-11 17:05:33 -0700

    Improved sample for clarity

--- sample.py
+++ sample.py
@@ -1,32 +1,55 @@
 from tripyagonal import *
 
-# How to construct a floating point matrix
-#m = TridiagonalMatrix(n=5,diag=1, sub=1, super=2)
-# m.pprint()
+# The parameters to the matrix are the following:
+# n : the size of of the matrix,
+# diag : entries on the diagonal of the matrix, provided in order,
+# sub : entries on the sub-diagonal of the matrix in order,
+# super: entries on the super-diagonal of the matrix, in order,
+# perturbed : a boolean value indicating whether to apply perturbation on the matrix.
 
-# How to construct an exact (stochastic) matrix with multiple entries per diagonal
-#m2 = TridiagonalMatrix(n=4,diag=['1/6'], sub=['1/2','1/3'], super=['1/3','1/2'], perturbed=True)
-#pprint(m2)
-#m2.pprint()
-# With just one entry, not perturbed
-m3 = TridiagonalMatrix(n=4 ,diag='1/6', sub=['1/2','7/12'], super=['1/3','1/4'])
-c_m3 = CayleyHamilton(m3)
-#pprint(c_m3.computeMatrixPower(3))
-print(c_m3.computeMatrixPower(10))
-#print(c_m3.stateProb(0,1,2))
-#print(c_m3.stateProb(0,1,2).evalf())
-#print(utility.latex(k_m2.eigenvalues()))
-#print(utility.latex(k_m2.eigenvectors()))
+# How to construct a floating point matrix:
 
-#c_m2 = CayleyHamilton(m2)
-# print(c_m2.stateProb(1,2,2))
-#pprint(c_m2.computeMatrixPower(2))
-#pprint(c_m2.computeMatrixPower(5))
+m = TridiagonalMatrix(n=5, diag=1, sub=1, super=2)
 
-#cm = CirculantMatrix(n=5, diag='1/6', sub='1/3', super='1/2')
-#cm.pprint()
+# Note that the entries are provided as floats
 
-# k_cm = Kouachi(cm)
+# How to construct an exact (stochastic) matrix with multiple entries per diagonal:
 
-# c_cm = CayleyHamilton(cm)
-# pprint(c_cm.computePower(6))
+m2 = TridiagonalMatrix(n=4, diag=['1/6'], sub=['1/2', '1/3'], super=['1/3', '1/2'], perturbed=True)
+
+# The entries here are provided as list of strings, and will be parsed by the sympy parser.
+# The entries will be recycled if insufficient entries are supplied, which can be taken advantage of to create
+# alternating patterns.
+
+# To compute eigenvalues and eigenvectors, select one of the available algorithms:
+# Kouachi
+# Cayley-Hamilton
+# Matrices that satisfy the conditions imposed by these algorithms can be processed as follows:
+
+k_m = Kouachi(m)
+c_m = CayleyHamilton(m2)
+
+# After applying these algorithms, we can request outputs with respective available functions, refer to each individual
+# algorithms for more info:
+
+k_m.eigenvalues()
+c_m.computeMatrixPower(10)
+
+# There are few ways to print the matrices to the console:
+
+# Pretty print
+pprint(m2)
+
+# Regular printing (with repr())
+print(m2)
+
+# Outputted as latex string
+print(latex(m2))
+
+# Refer to utility.py for more built-in tools
+
+# EXAMPLE:
+mat = TridiagonalMatrix(n=6, sub=['3/4', '3/8'], diag=['0'], super=['1/4', '1/2'], perturbed=True)
+c_m = CayleyHamilton(mat)
+print(c_m.eigenvalues())
+print(c_m.computeMatrixPower(13))
